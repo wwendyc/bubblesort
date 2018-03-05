9aeac2139f832cb69ed28f998f9a6a688bfa335a
wwendyc <wendy.changw@gmail.com>
2018-03-05 15:23:21 -0500

    split func done

--- mergesort.js
+++ mergesort.js
@@ -2,7 +2,13 @@ function split (arr) {
   const midPoint = Math.floor(arr.length / 2);
   const firstHalf = arr.slice(0, midPoint);
   const secondHalf = arr.slice(midPoint);
-  return [firstHalf, secondHalf];
+
+  if (arr.length === 1) {
+    return arr;
+  }
+  let finalFirstHalf = split(firstHalf);
+  let finalSecondHalf = split(secondHalf);
+  return [finalFirstHalf, finalSecondHalf];
 }
 
 function merge (arr) {
@@ -15,6 +21,10 @@ function merge (arr) {
   --> [[4, 5] | [1, 2, 3]]
   --> [1, 2, 3, 4, 5]
   */
-  if (arr.length === 1) return parseInt(arr);
+  let splitArr = split(arr)
+
   return arr;
 }
+
+let arr = [1, 2, 3];
+console.log(split(arr));
