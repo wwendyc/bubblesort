644c230c17424ae2a0a9c4f3b150bbc4c3af3064
hannahhopp <hannah.hopp@gmail.com>
2018-03-05 12:41:06 -0800

    trying to sort

--- mergesort.js
+++ mergesort.js
@@ -11,6 +11,33 @@ function split (arr) {
   return [finalFirstHalf, finalSecondHalf];
 }
 
+function comparisons (num1, num2) {
+  return num1 > num2;
+}
+
+function sort (arrayToSort) {
+// [[[3], [2]], [1]]]
+  return arrayToSort.reduce(function(sortedAcc, currentElem) {
+    let inner1 = currentElem[0]
+    let inner2 = currentElem[1]
+    switch (true) {
+      case (inner1.length > inner2.length):
+        sortedAcc.push(sort(inner1));
+        break;
+      case (inner2.length > inner1.length):
+        sortedAcc.push(sort(inner2));
+        break;
+      default:
+        if (comparisons(inner1, inner2)) currentElem = [inner2, inner1];
+        sortedAcc.push(currentElem);
+        break;
+    }
+    return sortedAcc;
+    // [[2, 3]]
+  }, [])
+
+}
+
 function merge (arr) {
   /*
   [5, 4, 3, 2, 1]
@@ -21,7 +48,9 @@ function merge (arr) {
   --> [[4, 5] | [1, 2, 3]]
   --> [1, 2, 3, 4, 5]
   */
-  let splitArr = split(arr)
+  let splitArr = split(arr);
+
+
 
   return arr;
 }
